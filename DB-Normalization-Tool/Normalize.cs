66a2829d480dff8628c0386ac26b22c89a5101a7
Omer <omer112254@gmail.com>
2021-11-06 18:08:39 +0200

    initializing project

---
+++ DB-Normalization-Tool/Normalize.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DB_Normalization_Tool
+{
+    public class Normalize
+    {
+        private List <string> CandidateKey;
+
+        private List <Dependency> FunctionalDependencies;
+
+        private Tuple<string, List<string>> CanonicalFunctionalDependencies;
+
+        private Boolean DependenciesLose;
+
+        private string GenerateCandidateKey()
+        {
+            return "CandidateKey";
+        }
+        private Tuple<string, List<string>> GenerateCanonicalFunctionalDependencies()
+        {
+            return "CandidateKey";
+        }
+
+    }
+}
